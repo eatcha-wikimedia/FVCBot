485d30883c646b0f7e5650543ff6bcae0b29ea04
I'm User:Eatcha <51746381+eatcha-wikimedia@users.noreply.github.com>
2019-06-19 18:32:34 +0530

    bug fix(error while counting videos in a gallery )

--- fvc.py
+++ fvc.py
@@ -716,6 +716,8 @@ class Candidate:
 
         # Find the number of lines in the gallery
         m = re.search(r"(?ms)<gallery>(.*)</gallery>", old_text)
+	if m is None:
+            return None
         count = m.group(0).count("\n")
 
         # We just need to append to the bottom of the gallery
